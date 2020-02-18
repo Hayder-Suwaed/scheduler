ecd28c3b938e97b41df996cf8b4b302f169a1422
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-18 16:25:32 -0700

    made  a request to retrieve interview from external source and this been handle by the Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from "react";
-import DayList from "components/DayList";
 import "components/Application.scss";
-import Appointment from "./Appointment";
-import getAppointmentsForDay from "../helpers/selectors";
+import React, { useEffect, useState } from "react";
+import Appointment from "components/Appointment";
+import DayList from "./DayList";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 import axios from "axios";
 
-
 export default function Application(props) {
-  const [state, setState] = useState({
+  let [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
- 
+
   const appointments = getAppointmentsForDay(state, state.day);
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
+
   useEffect(() => {
-    axios.all([
-      axios.get("http://localhost:4000/api/days"),
-      axios.get("http://localhost:4000/api/appointments")
-    ])
+    axios
+      .all([
+        axios.get("http://localhost:4000/api/days"),
+        axios.get("http://localhost:4000/api/appointments"),
+        axios.get("http://localhost:4000/api/interviewers")
+      ])
       .then((all) => {
-        setState(prev => ({
+        setState((prev) => ({
           ...prev,
           days: all[0].data,
-          appointments: all[1].data
-        }))
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }));
       })
-      .catch((error) => {
-        
-        // console.log(error.res.status);
-        // console.log(error.res.headers);
-        // console.log(error.res.data);
-      });
-  }, [])
 
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }, []);
+
+  console.log(appointments);
 
   return (
     <main className="layout">
@@ -55,17 +58,21 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => (
-          <Appointment
-            key={appointment.id}
-            id={appointment.id}
-            time={appointment.time}
-            interview={appointment.interview}
-          />
-        ))}
+        {appointments.map((e) => {
+          const interview = getInterview(state, e.interview);
+          return (
+            <Appointment
+              key={e.id}
+              {...e}
+              id={e.id}
+              time={e.time}
+              interview={interview}
+              // {...e}
+            />
+          );
+        })}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
-  }
-
+}
