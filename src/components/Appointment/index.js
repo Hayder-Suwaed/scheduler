acd4b694d7bd6d3726187d337bbf219148b72ee4
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-17 19:16:55 -0700

    added empty form

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.scss";
+
+// import Header from "./Header";
+// import Show from "./Show";
+// import Empty from "./Empty";
+
+const Appointment = ({ time, interview }) => {
+  return <article className="appointment"></article>;
+};
+
+export default Appointment;
