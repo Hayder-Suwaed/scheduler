f279acbb5a1c68eab221421b1bafae20ad9735d8
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-19 13:13:01 -0700

    usedd my custom hooks when props.interview has value then it will pass useVisualMode the sHOW mode which if it iis empty then show empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,38 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./styles.scss";
-import Header from "./Header";
-import Show from "./Show";
-import Empty from "./Empty";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview ? (<Show student={props.interview.student} interviewer={props.interview.interviewer.name} />) : (<Empty />)}
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form
+          name={""}
+          interviewer={""}
+          onCancel={() => back()}
+          interviewers={[]}
+          onSave={() => {}}
+        />
+      )}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      )}
     </article>
-  )
-} 
+  );
+}
