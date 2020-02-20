c1ffbbc5e8c3e30a8bb89db57772f9bc61d01834
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-20 13:35:23 -0700

    fixed bug that was showing error message when trying to delete an appointment

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -6,16 +6,24 @@ export default function useApplicationData() {
   const SET_DAY = "SET_DAY";
   const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
   const SET_INTERVIEW = "SET_INTERVIEW";
+  const SET_SPOTSREMAINING = "SET_SPOTSREMAINING";
 
   function reducer(state, action) {
     switch (action.type) {
       case SET_DAY:
-        return { ...state, day: action.value };
+        return { ...state, day: action.day };
       case SET_APPLICATION_DATA:
-        console.log("set-application type");
-        return { ...state, ...action.value };
+        return {
+          ...state,
+          days: action.days,
+          appointments: action.appointments,
+          interviewers: action.interviewers
+        };
       case SET_INTERVIEW: {
-        return { ...state, appointments: action.value };
+        return { ...state, appointments: action.appointments, spots: 5 };
+      }
+      case SET_SPOTSREMAINING: {
+        return { ...state, days: action.stateDays };
       }
       default:
         throw new Error(
@@ -23,88 +31,123 @@ export default function useApplicationData() {
         );
     }
   }
-
-  let [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
+    spots: 0,
     appointments: {},
-    interviewers: {}
+    interviews: {}
   });
 
-  useEffect(() => {
-    axios
-      .all([
-        axios.get("http://localhost:4000/api/days"),
-        axios.get("http://localhost:4000/api/appointments"),
-        axios.get("http://localhost:4000/api/interviewers")
-      ])
-      .then((all) => {
-        dispatch({
-          type: "SET_APPLICATION_DATA",
-          value: {
-            days: all[0].data,
-            appointments: all[1].data,
-            interviewers: all[2].data
-          }
-        });
+  const dayByAppId = (id) => {
+    let dayByAppointmenID = {};
+    state.days.forEach((item) => {
+      item.appointments.forEach((appointmentID) => {
+        if (id === appointmentID) {
+          dayByAppointmenID = { ...item };
+        }
       });
-  }, []);
-
-  //
-
-  const setDay = (day) =>
-    dispatch({
-      type: "SET_DAY",
-      value: day
     });
+    return dayByAppointmenID;
+  };
 
-  //
+  const setDay = (day) => dispatch({ type: SET_DAY, day });
 
   function bookInterview(id, interview) {
+    const daySpot = dayByAppId(id);
+    const spotIncrease = (daySpot) => {
+      const output = state.days.map((item, index) => {
+        if (index !== daySpot.id - 1) {
+          return item;
+        }
+        return {
+          ...daySpot,
+          spots: item.spots - 1
+        };
+      });
+      return output;
+    };
+
+    const stateDays = spotIncrease(daySpot);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
     return axios
-      .put(`http://localhost:4000/api/appointments/${id}`, { interview })
-      .then((resp) => {
-        const intvw = { ...interview };
+      .put(`http://localhost:4000/api/appointments/${id}`, appointment)
+      .then(
+        axios
+          .get("http://localhost:4000/api/days")
+          .then(
+            (res) => dispatch({ type: SET_INTERVIEW, appointments }),
+            dispatch({ type: SET_SPOTSREMAINING, stateDays })
+          )
+      );
+  }
 
-        const appointment = {
-          ...state.appointments[id],
-          interview: { ...intvw }
+  function cancel(id, interview) {
+    const daySpot = dayByAppId(id);
+
+    const spotIncrease = (daySpot) => {
+      const output = state.days.map((item, index) => {
+        if (index !== daySpot.id - 1) {
+          return item;
+        }
+        return {
+          ...daySpot,
+          spots: ++item.spots
         };
+      });
+      return output;
+    };
+    const stateDays = spotIncrease(daySpot);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
 
-        const appointments = {
-          ...state.appointments,
-          [id]: appointment
-        };
+    return axios
+      .delete(`http://localhost:4000/api/appointments/${id}`)
+      .then(
+        (res) => dispatch({ type: SET_INTERVIEW, appointments }),
+        dispatch({ type: SET_SPOTSREMAINING, stateDays })
+      );
+  }
 
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get(`http://localhost:4000/api/days`)),
+      Promise.resolve(axios.get(`http://localhost:4000/api/appointments`)),
+      Promise.resolve(axios.get(`http://localhost:4000/api/interviewers`))
+    ])
+      .then((res) => {
+        console.log(res);
         dispatch({
-          type: "SET_INTERVIEW",
-          value: appointments
+          type: SET_APPLICATION_DATA,
+          days: res[0].data,
+          appointments: res[1].data,
+          interviewers: res[2].data
         });
-      });
-  }
+      })
 
-  function cancel(id) {
-    return axios.delete(`/api/appointments/${id}`).then((resp) => {
-      const interview = {
-        ...state.appointments[id],
-        interview: null
-      };
-      const appointments = {
-        ...state.appointments,
-        [id]: interview
-      };
-
-      dispatch({
-        type: "SET_INTERVIEW",
-        value: appointments
-      });
-    });
-  }
+      .catch((error) => console.log(error));
+    return () => {};
+  }, []);
 
   return {
     state,
     setDay,
     bookInterview,
-    cancel: cancel
+    cancel
   };
 }
