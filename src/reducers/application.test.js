5b7bd1a528f60104082bab7abc937237884965bf
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-26 13:28:25 -0700

    fixing a bug with SAVINGfunction

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -1,5 +1,5 @@
 import { reducer } from "./application";
-
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 describe("Application Reducer", () => {
   it("thows an error with an unsupported type", () => {
     expect(() => reducer({}, { type: null })).toThrowError(
